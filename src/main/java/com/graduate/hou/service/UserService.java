fa0b05b6229075d78db618fc39613f42475d45d7
Huy16102002 <dangquanghuy16102002@gmail.com>
2024-09-30 22:11:31 +0700

    commit crud user

--- src/main/java/com/graduate/hou/service/UserService.java
+++ src/main/java/com/graduate/hou/service/UserService.java
@@ -1,4 +1,13 @@
 package com.graduate.hou.service;
 
+import com.graduate.hou.dto.UsersDTO;
+import com.graduate.hou.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> getAllUser();
+    User createUser(UsersDTO usersDTO);
+    User updateUser(Long id, UsersDTO usersDTO);
+    void deleteUser(Long id);
 }
